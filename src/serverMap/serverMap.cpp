0fcf554ea7737c7526d7021d1fb235386b5f4957
short-int <potash_is_K2CO3@163.com>
2023-07-13 17:20:24 +0800

    feat: provide statistics for in-game leaderboard
    Some functions in GlobalMap and ServerMap have changed. All `std::vector` parameters and return value of these functions now begin at 0.

--- src/serverMap/serverMap.cpp
+++ src/serverMap/serverMap.cpp
@@ -1,52 +1,52 @@
 #include "serverMap.h"
 
-void ServerMap::move(const int playerId, const Point start,
-                     const int deltaX, const int deltaY,
-                     const bool flag50p) {
-    Point end(start.x + deltaX, start.y + deltaY);
+bool
+ServerMap::move(const int playerId, const Point pntStart, const int deltaX, const int deltaY, const bool flagHalf) {
+    Point pntEnd(pntStart.x + deltaX, pntStart.y + deltaY);
 
-    if (end.x < 1 || end.x > width || end.y < 1 || end.y > length || abs(deltaX) + abs(deltaY) != 1)
-        return;
+    if (pntEnd.x < 1 || pntEnd.x > width || pntEnd.y < 1 || pntEnd.y > length || abs(deltaX) + abs(deltaY) != 1)
+        return false;
 
-    const Cell startInfo = info[start.x][start.y], endInfo = info[end.x][end.y];
+    const Cell cStart = info[pntStart.x][pntStart.y], cEnd = info[pntEnd.x][pntEnd.y];
 
-    if (!playerId || startInfo.belonging != playerId || endInfo.type == CellType::mountain)
-        return;
+    if (!playerId || cStart.belonging != playerId || cEnd.type == CellType::mountain)
+        return false;
 
-    const int moveNum = flag50p ? startInfo.number / 2 : startInfo.number - 1;
+    const int num = flagHalf ? cStart.number / 2 : cStart.number - 1;
 
-    if (moveNum < 0)
-        return;
+    if (num < 0)
+        return false;
 
-    info[start.x][start.y].number -= moveNum;
-    Cell *cell = &info[end.x][end.y];
+    info[pntStart.x][pntStart.y].number -= num;
+    Cell &cell = info[pntEnd.x][pntEnd.y];
 
-    if (!endInfo.belonging) {
-        *cell = Cell<%abs(moveNum - endInfo.number),
-                      moveNum > endInfo.number ? startInfo.belonging : endInfo.belonging,
-                      endInfo.type%>;
+    if (!cEnd.belonging) {
+        cell = Cell{abs(num - cEnd.number),
+                    num > cEnd.number ? cStart.belonging : 0,
+                    cEnd.type};
     } else {
-        if (teamInfo[startInfo.belonging] == teamInfo[endInfo.belonging]) {
-            *cell = Cell<%moveNum + endInfo.number,
-                          endInfo.type == CellType::crown ? endInfo.belonging : startInfo.belonging,
-                          endInfo.type%>;
+        if (teamInfo[cStart.belonging] == teamInfo[cEnd.belonging]) {
+            cell = Cell{num + cEnd.number,
+                        cEnd.type == CellType::crown ? cEnd.belonging : cStart.belonging,
+                        cEnd.type};
         } else {
-            if (moveNum <= endInfo.number) {
-                *cell = Cell<%endInfo.number - moveNum, endInfo.belonging, endInfo.type%>;
+            if (num <= cEnd.number) {
+                cell = Cell{cEnd.number - num, cEnd.belonging, cEnd.type};
             } else {
-                switch (endInfo.type) {
+                switch (cEnd.type) {
                     case CellType::land:
                     case CellType::castle: {
-                        *cell = Cell<%moveNum - endInfo.number, startInfo.belonging, endInfo.type%>;
+                        cell = Cell{num - cEnd.number, cStart.belonging, cEnd.type};
                         break;
                     }
                     case CellType::crown: {
-                        *cell = Cell<%moveNum - endInfo.number, startInfo.belonging, CellType::castle%>;
+                        cell = Cell{num - cEnd.number, cStart.belonging, CellType::castle};
                         for (int i = 1; i <= width; i++)
                             for (int j = 1; j <= length; j++)
-                                if (info[i][j].belonging == endInfo.belonging)
-                                    info[i][j] = Cell<%(info[i][j].number + 1) / 2, startInfo.belonging,
-                                                            info[i][j].type%>;
+                                if (info[i][j].belonging == cEnd.belonging)
+                                    info[i][j] = Cell{(info[i][j].number + 1) / 2, cStart.belonging,
+                                                      info[i][j].type};
+                        roundLose[cEnd.belonging - 1] = round;
                         break;
                     }
                     default:;
@@ -54,6 +54,11 @@ void ServerMap::move(const int playerId, const Point start,
             }
         }
     }
+    return true;
+}
+
+void ServerMap::calcStat() {
+    GlobalMap::calcStat(roundLose);
 }
 
 void ServerMap::addRound() {
@@ -69,13 +74,60 @@ void ServerMap::addRound() {
                 if (info[i][j].belonging)
                     info[i][j].number++;
 
-    std::vector<bool>lost(crownCnt+1);
-    calcStat();
-    for(int i=1;i<=width;i++) {
-        for(int j=1;j<=length;j++) {
+    GlobalMap::calcStat(roundLose);
+    round++;
+}
 
-        }
+
+void ServerMap::surrender(int id) {
+    roundLose[id - 1] = round;
+}
+
+ServerMap::ServerMap(const GlobalMap &globMap) : GlobalMap(globMap) {
+    roundLose = std::vector<int>(crownCnt, INT_MAX);
+}
+
+std::string ServerMap::export2Str(bool fetchFullInfo) {
+    using std::string;
+    using std::vector;
+
+    vector<int> numbers;
+
+    numbers.push_back(fetchFullInfo);
+    if (fetchFullInfo) {
+        numbers.push_back(width);
+        numbers.push_back(length);
+        numbers.push_back(teamCnt);
+        numbers.push_back(crownCnt);
+        for (const auto i: teamInfo)
+            numbers.push_back(i);
     }
 
-    round++;
+    numbers.push_back(round);
+    for (auto i: roundLose)
+        numbers.push_back(i);
+    for (int i = 1; i <= width; i++)
+        for (int j = 1; j <= length; j++) {
+            switch (info[i][j].type) {
+                case CellType::land:
+                    numbers.push_back(0);
+                    break;
+                case CellType::crown:
+                    numbers.push_back(1);
+                    break;
+                case CellType::castle:
+                    numbers.push_back(2);
+                    break;
+                case CellType::mountain:
+                    numbers.push_back(3);
+                    break;
+            }
+            numbers.push_back(info[i][j].belonging);
+            numbers.push_back(info[i][j].number);
+        }
+
+    string result;
+    for (auto i: numbers)
+        result.append(std::to_string(i) + '_');
+    return result;
 }
