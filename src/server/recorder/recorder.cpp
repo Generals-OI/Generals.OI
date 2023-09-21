ec2c4facc4cc81acce6ece3c85c80402f4004bd7
zbe07 <2695385831@qq.com>
2023-09-21 11:10:34 +0800

    feat: update `Recorder`

--- src/server/recorder/recorder.cpp
+++ src/server/recorder/recorder.cpp
@@ -2,9 +2,6 @@
 #include <QDebug>
 
 bool Recorder::addRecord(int id, int x, int y, int dx, int dy, bool flag) {
-    if (!isAddName) {
-        return false;
-    }
     bool b[19] = {};
     if (id == -1) { // 回合分隔符
         std::fill(b, b + 19, true);
@@ -74,104 +71,110 @@ QByteArray Recorder::exportRecords() {
 bool Recorder::importRecord(QByteArray &record) {
     data.clear();
     data = record;
+    players.clear();
+    moves.clear();
+    auto it = data.begin();
+    gameMode = 0;
+    for (int i = 3; i >= 0; i--) {
+        gameMode += *it << (i * 8);
+        it++;
+    }
+    n = *it;
+    it++;
+    for (int i = 1; i <= n; i++) {
+        int idTeam = *it;
+        it++;
+
+        QByteArray name;
+        while (*it != '\0') {
+            name.append(*it);
+            it++;
+        }
+
+        players.append({QString::fromLocal8Bit(name), idTeam});
+        it++;
+    }
+
+    QVector<RecordInfo> roundMoves;
+    RecordInfo move;
+
+    bool b[19] = {};
+    while (true) {
+        for (int i = 0; i < 19; i++) {
+            if (pos == 0) {
+                if (it == data.end())
+                    goto end;
+                c = *it;
+                it++;
+            }
+            b[i] = (c & (1 << (7 - pos))) > 0;
+            if (++pos > 7) {
+                pos = 0;
+            }
+        }
+
+        bool isSep = true; // 回合分隔符标志
+        for (int i = 0; i < 19; i++) {
+            if (!b[i]) {
+                isSep = false;
+                break;
+            }
+            //qDebug()<<i<<b[i];
+        }
+        if (isSep) {
+            moves.append(roundMoves);
+            roundMoves.clear();
+        } else {
+            move.startX = move.startY = move.direction = move.idPlayer = 0;
+            for (int i = 5, j = 1; i >= 0; i--) {
+                if (b[i]) {
+                    move.startX = move.startX + j;
+                }
+                j = j * 2;
+            }
+            move.startX++;
+            for (int i = 11, j = 1; i >= 6; i--) {
+                if (b[i]) {
+                    move.startY = move.startY + j;
+                }
+                j = j * 2;
+            }
+            move.startY++;
+            for (int i = 15, j = 1; i >= 12; i--) {
+                if (b[i]) {
+                    move.idPlayer = move.idPlayer + j;
+                }
+                j = j * 2;
+            }
+            move.idPlayer++;
+            if (!b[16] && !b[17]) {
+                move.direction = 0;
+            } else if (!b[16] && b[17]) {
+                move.direction = 2;
+            } else if (b[16] && !b[17]) {
+                move.direction = 1;
+            } else if (b[16] && b[17]) {
+                move.direction = 3;
+            } else continue;
+            move.flag50p = b[18];
+            roundMoves.append(move);
+        }
+    }
+
+    end:
+    if (!roundMoves.empty())
+        moves.append(roundMoves);
     return true;
 }
 
-bool Recorder::getNextRecord(int &id, int &x, int &y, int &dx, int &dy, bool &flag) {
-    bool b[19] = {}, isEnd = false;
-
-    for (int i = 0; i < 19; i++) {
-        if (pos == 0) {
-            if (++p >= data.size()) {
-                isEnd = true;
-                c = '\0';
-                data.clear();
-                p = 0;
-                break;
-            }
-            c = data.at(p);
-            //qDebug()<<'!'<<int(c);
-        }
-        if (c & (1 << (7 - pos))) {
-            b[i] = true;
-        } else {
-            b[i] = false;
-        }
-        if (++pos > 7) {
-            pos = 0;
-        }
+void Recorder::init(QVector<QPair<QString, int>> playerInfo, int mode) {
+    for (int i = 0; i <= 3; i++) {
+        data.append(char(mode >> 24));
+        mode <<= 8;
     }
-    bool isSep = true; // 回合分隔符标志
-    for (int i = 0; i < 19; i++) {
-        if (!b[i]) {
-            isSep = false;
-        }
-        //qDebug()<<i<<b[i];
-    }
-    if (isSep) {
-        id = -1;
-    } else {
-        for (int i = 5, j = 1; i >= 0; i--) {
-            if (b[i]) {
-                x = x + j;
-            }
-            j = j * 2;
-        }
-        x++;
-        for (int i = 11, j = 1; i >= 6; i--) {
-            if (b[i]) {
-                y = y + j;
-            }
-            j = j * 2;
-        }
-        y++;
-        for (int i = 15, j = 1; i >= 12; i--) {
-            if (b[i]) {
-                id = id + j;
-            }
-            j = j * 2;
-        }
-        id++;
-        if (!b[16] && !b[17]) {
-            dx = -1, dy = 0;
-        } else if (!b[16] && b[17]) {
-            dx = 1, dy = 0;
-        } else if (b[16] && !b[17]) {
-            dx = 0, dy = -1;
-        } else if (b[16] && b[17]) {
-            dx = 0, dy = 1;
-        }
-        flag = b[18];
-    }
-    return isEnd;
-}
-
-void Recorder::addName(QVector<QString> name) {
-    n = (int) name.size();
-    data.append(char(n * 5 + 1));
+    n = (int) playerInfo.size();
+    data.append(char(n));
     for (int i = 0; i < n; i++) {
-        data.append(name[i].toLocal8Bit());
-        data.append('\0');
+        data.append(char(playerInfo[i].second)).append(playerInfo[i].first.toLocal8Bit()).append('\0');
     }
-    isAddName = true;
 }
-
-void Recorder::getName(QVector<QString> &name) {
-    name.clear();
-    n = (data.at(0) - 1) / 5;
-    p = 1;
-    name.push_back("");
-    for (int i = 1; i <= n; p++) {
-        if (data.at(p) == '\0') {
-            if (i < n) {
-                name.push_back("");
-            }
-            i++;
-        } else {
-            name[name.size() - 1] += data.at(p);
-        }
-    }
-    p--;
-    isAddName = false;
-}
-
--- src/server/recorder/recorder.h
+++ src/server/recorder/recorder.h
@@ -5,33 +5,34 @@
 #include <algorithm>
 #include <QVector>
 
+#include "gameInformation.h"
+
 class Recorder {
 public:
-    /// add name
-    void addName(QVector<QString> name);
+    void init(QVector<QPair<QString, int>> playerInfo, int mode);
 
-    /// get name
-    void getName(QVector<QString> &name);
-
-    ///添加一行数据
     bool addRecord(int id, int x, int y, int dx, int dy, bool flag);
 
-    ///加密
     QByteArray exportRecords();
 
-    ///解密，并返回是否成功
     bool importRecord(QByteArray &record);
-
-    ///读取下一行数据（需先解密）
-    bool getNextRecord(int &id, int &x, int &y, int &dx, int &dy, bool &flag);
 
 private:
     QByteArray data{};
-    int pos{}, p = {};
+    int pos{};
     char c{};
     int n{};
-    bool isAddName{};
+
+public:
+    QVector<QPair<QString, int>> players;
+    struct RecordInfo;
+    QVector<QVector<RecordInfo>> moves;
+    int gameMode{};
 };
 
+struct Recorder::RecordInfo {
+    int startX{}, startY{}, direction{}, idPlayer{};
+    bool flag50p{};
+};
 
 #endif // RECORDER_H
