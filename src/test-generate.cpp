69e89c7a9fda0abed703d55272377b83b45ac5a4
x405x <x405x@qq.com>
2023-09-23 16:33:22 +0800

    chore: updated some files for debugging

--- src/test-generate.cpp
+++ src/test-generate.cpp
@@ -1,12 +1,22 @@
 #include "serverMap.h"
 #include "mapGenerator.h"
 #include <algorithm>
+#include <windows.h>
 
 int main() {
     using namespace std;
 
     vector<int> idTeam{1, 2, 2, 3, 3, 4, 4};
-    ServerMap servMap = RandomMapGenerator::randomMap(7, 4, idTeam, 1);
-    servMap.print();
-    cout << endl << endl;
+    ServerMap map1 = RandomMapGenerator::randomMap(7, 4, idTeam, 1);
+    QByteArray byteArray = map1.toByteArray();
+    qDebug() << byteArray;
+    ServerMap map2;
+    map2.loadByteArray(byteArray);
+
+    Sleep(1000);
+
+    map1.print();
+    puts("===");
+    map2.print();
+    puts("===");
 }
--- src/test-recorder.cpp
+++ src/test-recorder.cpp
@@ -1,29 +1,67 @@
 #include "recorder.h"
+#include "serverMap.h"
 #include <iostream>
 #include <QDebug>
+#include <QFile>
+#include <windows.h>
 
 int main() {
-    Recorder r;
-    int id, x, y, dx, dy;
-    bool flag;
-    id = 1, x = 1, y = 1, dx = 1, dy = 0, flag = 0;
-    r.addRecord(id, x, y, dx, dy, flag);
-    id = 1, x = 1, y = 1, dx = 0, dy = 1, flag = 1;
-    r.addRecord(id, x, y, dx, dy, flag);
-    id = 16, x = 32, y = 13, dx = -1, dy = 0, flag = 0;
-    r.addRecord(id, x, y, dx, dy, flag);
-    id = 3, x = 45, y = 54, dx = 0, dy = 1, flag = 1;
-    r.addRecord(id, x, y, dx, dy, flag);
-    //qDebug() << r.exportRecords();
-    QByteArray _r = r.exportRecords();
-    if (r.importRecord(_r)) {
-        id = x = y = dx = dy = 0;
-        flag = false;
-        while (!r.getNextRecord(id, x, y, dx, dy, flag)) {
-            qDebug() << id << x << y << dx << dy << flag;
-            id = x = y = dx = dy = 0;
-            flag = false;
-        }
-        //qDebug() << id << x << y << dx << dy << flag;
+    Recorder r1, r2;
+    QVector<QPair<QString, int>> players{{"First",  1},
+                                         {"Second", 2}};
+    r1.init(players);
+
+    r1.addRecord(1, 10, 8, -1, 0, false);
+    r1.addRecord(2, 15, 7, 1, 0, true);
+    r1.addRecord(-1, 0, 0, 0, 0, false);
+
+    r1.addRecord(1, 10, 11, 1, 0, true);
+    r1.addRecord(2, 15, 43, 0, -1, true);
+    r1.addRecord(-1, 0, 0, 0, 0, false);
+
+    r1.addRecord(1, 6, 8, -1, 0, false);
+    r1.addRecord(2, 8, 9, 0, 1, false);
+    r1.addRecord(-1, 0, 0, 0, 0, false);
+
+    r1.addRecord(1, 12, 15, 1, 0, true);
+    r1.addRecord(2, 17, 19, 1, 0, true);
+    r1.addRecord(-1, 0, 0, 0, 0, false);
+
+    auto ba = r1.exportRecords();
+    int cnt = 0;
+    for (auto i: ba)
+        qDebug() << (cnt++) << (unsigned) (i) << i;
+    r2.importRecord(ba);
+
+    for (const auto &player: r2.players)
+        qDebug() << player.first << player.second;
+    for (const auto &round: r2.moves) {
+        qDebug() << "===";
+        for (const auto &move: round)
+            qDebug() << move.idPlayer << move.startX << move.startY << direction4[move.direction][0]
+                     << direction4[move.direction][1] << move.flag50p;
     }
+
+    /*QFile file("D:/replay.rp");
+    if (file.open(QIODevice::ReadOnly)) {
+        QByteArray ba = file.readAll();
+        file.close();
+        ServerMap map;
+        map.loadByteArray(ba);
+        map.print();
+        puts("===\n===");
+        if (r.importRecord(ba)) {
+            for (auto i:r.players) {
+                std::cout << i.first.toStdString() << ' ' << i.second << std::endl;
+            }
+            for (auto i:r.moves) {
+                for (auto j:i) {
+                    std::cout << j.idPlayer << ' ' << j.startX << ' ' << j.startY << ' ' << j.direction << ' '
+                              << j.flag50p << std::endl;
+                }
+            }
+        }
+    }
+*/
+
 }
